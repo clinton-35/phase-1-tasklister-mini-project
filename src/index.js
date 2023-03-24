222f370fa14add1407bfcbb660eca5130f353de8
clinton <clinton.wambua@student.moringaschool.com>
2023-03-24 11:37:48 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    // Get the button and the paragraph elements
+    const button = document.getElementById("myButton");
+    const paragraph = document.getElementById("myParagraph");
+  
+    // Add an event listener to the button
+    button.addEventListener("click", () => {
+      // Toggle the visibility of the paragraph
+      if (paragraph.style.display === "none") {
+        paragraph.style.display = "block";
+      } else {
+        paragraph.style.display = "none";
+      }
+    });
+
 });
